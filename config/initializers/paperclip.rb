bde3e41f61734261ffe3247d9e0f4d1557fb9b75
q0w0qeeq <3257977895@qq.com>
2024-03-27 09:42:44 +0800

    Update paperclip.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -61,8 +61,8 @@ if ENV['S3_ENABLED'] == 'true'
 
     s3_options: {
       signature_version: ENV.fetch('S3_SIGNATURE_VERSION') { 'v4' },
-      http_open_timeout: ENV.fetch('S3_OPEN_TIMEOUT') { '5' }.to_i,
-      http_read_timeout: ENV.fetch('S3_READ_TIMEOUT') { '5' }.to_i,
+      http_open_timeout: ENV.fetch('S3_OPEN_TIMEOUT') { '600' }.to_i,
+      http_read_timeout: ENV.fetch('S3_READ_TIMEOUT') { '600' }.to_i,
       http_idle_timeout: 5,
       retry_limit: 0,
     }
